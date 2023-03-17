d8e5fb2b6c4dcf1c3025786c967145575d345a0b
SilviaAdris <silvia.adris98@outlook.com>
2023-03-17 14:15:50 -0500

    update

--- game.js
+++ game.js
@@ -1,59 +1,117 @@
-function computerPlay(){ 
-    let random =  Math.floor(Math.random() * 3) + 1;
-    if( random === 1){
-        return "rock";
-    }
-    else if (random === 2){
-        return "paper";
-    }
-    else{
-        return "scissors"; 
-    }
-}
+const game = () => {
+    let playerScore = 0;
+	let computerScore = 0;
+	let moves = 0;
 
-function playRound(playerSelection, computerSelection){
-    let lower = playerSelection.toLowerCase();
-    console.log(playerSelection +" "+ computerSelection);
-    if (lower === "rock" & computerSelection === "paper"){
-        return "You Lose! Paper beats Rock!"
-    }
-    else if (lower === "rock" & computerSelection === "scissors"){
-        return "You win! Good Job!"
-    }
-    else if (lower === "paper" & computerSelection === "rock"){  
-        return "You win! Good Job!";
-    }
-    else if (lower === "paper" & computerSelection === "scissors"){
-        return "You lose! Scissors beats Paper!";
-    }
-    else if (lower === "scissors" & computerSelection === "rock"){
-        return "You lose! Rock beats Scissors!";
-    }
-    else if (lower === "scissors" & computerSelection === "paper"){
-        return "You win! Good Job!";
-    }
-    else {
-        return "It's a tie!";
+	const playGame = () => {
+		const rockBtn = document.querySelector('.rock');
+		const paperBtn = document.querySelector('.paper');
+		const scissorBtn = document.querySelector('.scissor');
+		const playerOptions = [rockBtn,paperBtn,scissorBtn];
+		const computerOptions = ['rock','paper','scissors'];
+
+    const playRound = (player,computer) => {
+            const result = document.querySelector('.result');
+            const playerScoreBoard = document.querySelector('.p-count');
+            const computerScoreBoard = document.querySelector('.c-count');
+            player = player.toLowerCase();
+            computer = computer.toLowerCase();
+            if(player === computer){
+                result.textContent = 'Tie';
+            }
+            else if(player === 'rock'){
+                if(computer === 'paper'){
+                    result.textContent = 'Computer Won :(';
+                    computerScore++;
+                    computerScoreBoard.textContent = computerScore;
+    
+                }else{
+                    result.textContent = 'Player Won!';
+                    playerScore++;
+                    playerScoreBoard.textContent = playerScore;
+                }
+            }
+            else if(player === 'scissors'){
+                if(computer === 'rock'){
+                    result.textContent = 'Computer Won :(';
+                    computerScore++;
+                    computerScoreBoard.textContent = computerScore;
+                }else{
+                    result.textContent = 'Player Won!';
+                    playerScore++;
+                    playerScoreBoard.textContent = playerScore;
+                }
+            }
+            else if(player === 'paper'){
+                if(computer === 'scissors'){
+                    result.textContent = 'Computer Won :(';
+                    computerScore++;
+                    computerScoreBoard.textContent = computerScore;
+                }else{
+                    result.textContent = 'Player Won!';
+                    playerScore++;
+                    playerScoreBoard.textContent = playerScore;
+                }
+            }
     } 
-}
+    function playingGame(){
+            const movesLeft = document.querySelector('.movesleft');
+            moves++;
+            movesLeft.innerText = `Moves Left: ${10-moves}`;
+            const choiceNumber = Math.floor(Math.random()*3);
+            const computerChoice = computerOptions[choiceNumber];
+            playRound(this.innerText,computerChoice);
+            if(moves == 10){
+                gameOver(playerOptions,movesLeft);
+            }
+    }
+	for (var i = 0 ; i < playerOptions.length; i++) {
+			playerOptions[i].addEventListener('click', playingGame, false);
+        }
+    }
+	const gameOver = (playerOptions,movesLeft) => {
+        const endGame = document.querySelector('.end-game');
+		const chooseMove = document.querySelector('.move');
+		const result = document.querySelector('.result');
+		const reloadBtn = document.querySelector('.reload');
 
-function game(){
-    let score = 0;
-    for (let i = 0 ; i < 5; i++){
-        let playerSelection = prompt("What's your move?");
-        let computerSelection = computerPlay();
-        let play_round = playRound(playerSelection, computerSelection);
-        console.log(play_round);
-        if (play_round == "You win! Good Job!"){
-            score++; 
-        }
-        else if (play_round == "It's a tie!"){
-            i--;
-        }
-    }
-    if (score >= 3){
-        return "Winner!";
-    }
-    return "Loser!"
+		playerOptions.forEach(option => {
+			option.style.display = 'none';
+		})
+		chooseMove.innerText = 'Game Over!!';
+		movesLeft.style.display = 'none';
+		if(playerScore > computerScore){
+            result.style['justify-content'] = 'center';
+			result.style.fontSize = '2rem';
+			result.innerText = 'You Won The Game.';
+			result.style.color = 'black';
+            reloadBtn.innerText = 'Restart';
+		    reloadBtn.style.display = 'flex';
+            reloadBtn.style['justify-content'] = 'center';
+		}
+		else if(playerScore < computerScore){
+            result.style['justify-content'] = 'center';
+			result.style.fontSize = '2rem';
+			result.innerText = 'You Lost The Game.';
+			result.style.color = 'black';
+            reloadBtn.innerText = 'Restart';
+		    reloadBtn.style.display = 'flex';
+            reloadBtn.style['justify-content'] = 'center';
+		}
+		else{
+            result.style['justify-content'] = 'center';
+			result.style.fontSize = '2rem';
+			result.innerText = 'Tie';
+			result.style.color = 'black';
+            reloadBtn.innerText = 'Restart';
+		    reloadBtn.style.display = 'flex';
+            reloadBtn.style['justify-content'] = 'center';
+		}
+		reloadBtn.addEventListener('click',() => {
+		window.location.reload();
+		})
+	}
+	playGame();	
 }
-document.write(game());
+game();
+
